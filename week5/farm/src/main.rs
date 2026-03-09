161f61ddcf2676cc60d37a6da852ff987d5ff9d5
somnambulist <2400013511@stu.pku.edu.cn>
2026-03-09 06:25:07 +0000

    Week5

--- week5/farm/src/main.rs
+++ week5/farm/src/main.rs
@@ -72,11 +72,27 @@ fn main() {
     let start = Instant::now();
 
     // TODO: call get_input_numbers() and store a queue of numbers to factor
+    let numbers = Arc::new(Mutex::new(get_input_numbers()));
 
-    // TODO: spawn `num_threads` threads, each of which pops numbers off the queue and calls
-    // factor_number() until the queue is empty
+    let mut handles = Vec::with_capacity(num_threads);
+    for _ in 0..num_threads {
+        let numbers = Arc::clone(&numbers);
+        handles.push(thread::spawn(move || loop {
+            let next = {
+                let mut queue = numbers.lock().unwrap();
+                queue.pop_front()
+            };
 
-    // TODO: join all the threads you created
+            match next {
+                Some(num) => factor_number(num),
+                None => break,
+            }
+        }));
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
 
     println!("Total execution time: {:?}", start.elapsed());
 }
